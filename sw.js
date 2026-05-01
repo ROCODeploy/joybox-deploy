e60b203698d4c16dddee0f7d063af2d4f6fdabd5
Rosano <git@xeari.com>
2026-05-01 11:06:32 +0200

    Build 4037e7f

--- sw.js
+++ sw.js
@@ -31,7 +31,7 @@
 
 			// DATA
 
-			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1774510649026',
+			_DataVersionCacheName: 'OLSKServiceWorkerVersionCache-1777626376614',
 			_DataPersistenceCacheName: 'OLSKServiceWorkerPersistenceCache',
 			_DataOriginPage: '/play',
 
